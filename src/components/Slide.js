2a004c757645860358d8174b50bf61249d6ee466
Darren Emory <darren.emory@inspireinvesting.com>
2022-06-10 21:38:24 -0600

    moving axios call to slideshow component, passing url as props

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,33 +1,14 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-
-function Slide() {
-  const [imgPath, setImgPath] = useState("");
-  const fetchImgPath = () => {
-    axios({
-      method: "get",
-      url: `https://api.thecatapi.com/v1/images/search?size=full`,
-      params: {
-        // api_key: process.env.REACT_APP_cat_api_key,
-      },
-    }).then((res) => {
-      setImgPath(res.data[0].url);
-    });
-  };
-
-  useEffect(() => {
-    fetchImgPath();
-  }, []);
-
+function Slide(props) {
   return (
     <div
       className="slideContainer"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(${imgPath})`,
+        backgroundImage: `url(${props.imgPath})`,
+        position: "absolute",
         display: "flex",
-        width: "50vw",
-        height: "50vh",
+        width: "100vw",
+        height: "100vh",
       }}
     ></div>
   );
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,14 +1,32 @@
 import Slide from "./Slide";
-import { useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Slideshow() {
-  // move axios to this component to get multi-line results and then map thru <Slide /> component and passing URL
+  const [allImgs, setAllImgs] = useState([]);
+  const fetchImgPath = () => {
+    axios({
+      method: "get",
+      url: `https://api.thecatapi.com/v1/images/search?limit=3`,
+      header: {
+        api_key: process.env.REACT_APP_cat_api_key,
+      },
+    }).then((res) => {
+      setAllImgs(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchImgPath();
+  }, []);
+
   // useEffect setInterval to go thru array and fade out one at a time
   return (
     <div>
-      <Slide />
-      <Slide />
-      <Slide />
+      {allImgs.map((slide) => {
+        return <Slide imgPath={slide.url} />;
+      })}
+      <Slide imgPath="" />
     </div>
   );
 }
